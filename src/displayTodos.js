8aaf845f1ba6d9bbce4a995a51a79369e8318e52
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-16 16:40:07 -0700

    Add scrolling and styling for main todos

--- src/displayTodos.js
+++ src/displayTodos.js
@@ -2,18 +2,20 @@ import Todo from './todo.js'
 
 const todoArr = [new Todo('steve', 'a', 'v', 'v', 'notes')];
 
-const content = document.getElementById('current');
+const content = document.getElementById('todos');
 export default function makeCard() {
-    for (const i in todoArr){
+    for (const item of todoArr){
         const element = document.createElement('div');
         element.classList.add('card');
 
-        for (const prop in i) {
+        for (const prop in item) {
             const component = document.createElement('div');
-            component.innerHTML = i[prop];
+            component.innerHTML = item[prop];
             element.appendChild(component);
         }
 
         content.appendChild(element);
     }
 }
+
+//send out signal that new todo created event
