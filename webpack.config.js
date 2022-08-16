8f52d9a49fe59ddbe2d6b3c897e53b027da5d20f
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-16 11:50:27 -0700

    Set up workspace

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Todo App', 
+            filename: 'index.html',
+            template: 'src/template.html'
+        }),
+    ]
+}
