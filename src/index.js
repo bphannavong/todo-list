0bd8a69b32db1257cdba048ceabd45d4b4325a19
Billy Phannavong <billy.phannavong@gmail.com>
2022-08-17 09:11:12 -0700

    Add modal for task adding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import _ from './todo.js';
 import x from './displayTodos.js';
+import './modal.js'
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', x);
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', x);
---
+++ src/modal.js
@@ -0,0 +1,27 @@
+import makeCard from './displayTodos.js';
+
+const modal = document.getElementById('taskModal');
+const closeModal = document.querySelector('.close');
+const addBtn = document.getElementById('addBtn');
+const submitBtn = document.getElementById('submitBtn');
+
+addBtn.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+submitBtn.addEventListener('click', makeCard);
+submitBtn.addEventListener('click', function() {
+    modal.style.display = 'none'
+});
+
+window.addEventListener('click', function (e) { //if window is clicked on modal (not modal content) then close modal
+    console.log(e.target);
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+});
+
